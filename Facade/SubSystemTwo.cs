b6585cdad4e4c393810eff82fdbdd47a59d88a46
Mohammed Tanbir Hossain <pasic.tanbir@hotmail.com>
2019-07-09 13:25:39 +0900

    Add project files.

---
+++ Facade/SubSystemTwo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Facade
+{
+    public class SubSystemTwo
+    {
+        public void MethodTwo()
+        {
+            Console.WriteLine(" SubsystemTwo Method");
+
+        }
+    }
+}
